919ae174567c176ae9054a8c67875b57d355041c
UncleSema <semenkorobko2@gmail.com>
2022-05-01 00:05:17 +0300

    Add basic Thymeleaf functionality

--- src/main/java/ru/unclesema/ttb/controller/ApplicationController.java
+++ src/main/java/ru/unclesema/ttb/controller/ApplicationController.java
@@ -2,10 +2,10 @@ package ru.unclesema.ttb.controller;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import ru.unclesema.ttb.analyze.SimulationRequest;
 import ru.unclesema.ttb.analyze.SimulationResult;
 import ru.unclesema.ttb.service.ApplicationService;
 
@@ -13,24 +13,28 @@ import java.time.LocalDateTime;
 import java.time.ZoneOffset;
 import java.util.List;
 
-@RestController
+@Controller
 @RequiredArgsConstructor
 public class ApplicationController {
     private final ApplicationService service;
 
     @PostMapping("/simulate")
-    public SimulationResult simulate(@RequestBody Request request) {
-        return service.simulate(request.figiList(), request.from().toInstant(ZoneOffset.UTC), request.to().toInstant(ZoneOffset.UTC));
+    public String simulate(@ModelAttribute SimulationRequest request, Model model) {
+        System.err.println(request);
+        SimulationResult result = service.simulate(request.getFigiList(), request.getFrom().atStartOfDay().toInstant(ZoneOffset.UTC), request.getTo().atStartOfDay().toInstant(ZoneOffset.UTC));
+        System.err.println(result);
+        return "result";
     }
 
     @GetMapping("/figi")
+    @ResponseBody
     public String figi() {
         return service.figi();
     }
-}
 
-record Request(List<String> figiList,
-               @JsonFormat(pattern = "yyyy-MM-dd HH:mm") LocalDateTime from,
-               @JsonFormat(pattern = "yyyy-MM-dd HH:mm") LocalDateTime to) {
-
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("request", new SimulationRequest());
+        return "index";
+    }
 }
